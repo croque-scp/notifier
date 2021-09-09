2f4c2bce05cd49e10cf5e5ad369cffa9b3b108e4
Ross Williams <rossjrw@gmail.com>
2021-09-09 22:39:20 +0100

    Use MySQL data type in table definitions
    Also document some database practices used in this project.

--- notifier/database/queries/create_tables.script.sql
+++ notifier/database/queries/create_tables.script.sql
@@ -1,58 +1,69 @@
+--
+-- Foreign key constraints are rarely used in this schema - while the
+-- values of some columns do map to the values of others, it is not
+-- important for all values to be represented in the corresponding table.
+-- It would not make sense not to record posts made by a user just because
+-- that user does not have a notification config, for example.
+--
+-- VARCHAR limits where appropriate have been selected to match the maximum
+-- length of the corresponding value permitted by Wikidot.
+--
+
 CREATE TABLE IF NOT EXISTS user_config (
-  user_id TEXT NOT NULL PRIMARY KEY,
-  username TEXT NOT NULL,
-  frequency TEXT NOT NULL,
-  language TEXT NOT NULL,
-  delivery TEXT NOT NULL
+  user_id VARCHAR(20) NOT NULL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  frequency VARCHAR(10) NOT NULL,
+  language VARCHAR(5) NOT NULL,
+  delivery VARCHAR(5) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS user_last_notified (
-  user_id TEXT NOT NULL PRIMARY KEY,
-  notified_timestamp INTEGER NOT NULL
+  user_id VARCHAR(20) NOT NULL PRIMARY KEY,
+  notified_timestamp INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE manual_sub (
-  user_id TEXT NOT NULL REFERENCES user_config (user_id),
-  thread_id TEXT NOT NULL,
-  post_id TEXT,
-  sub INTEGER NOT NULL CHECK (sub IN (-1, 1)),
+  user_id VARCHAR(20) NOT NULL REFERENCES user_config (user_id),
+  thread_id VARCHAR(20) NOT NULL,
+  post_id VARCHAR(20),
+  sub TINYINT NOT NULL CHECK (sub IN (-1, 1)),
   UNIQUE (user_id, thread_id, post_id, sub)
 );
 
 CREATE TABLE IF NOT EXISTS global_override (
-  wiki_id TEXT NOT NULL,
-  override_settings_json TEXT NOT NULL
+  wiki_id VARCHAR(50) NOT NULL,
+  override_settings_json VARCHAR(2000) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS wiki (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL,
-  secure INTEGER NOT NULL CHECK (secure IN (0, 1))
+  id VARCHAR(50) NOT NULL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  secure TINYINT(1) NOT NULL CHECK (secure IN (0, 1))
 );
 
 CREATE TABLE IF NOT EXISTS category (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL
+  id VARCHAR(20) NOT NULL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS thread (
-  id TEXT NOT NULL PRIMARY KEY,
-  title TEXT NOT NULL,
-  wiki_id TEXT NOT NULL,
-  category_id TEXT,
-  creator_username TEXT,
-  created_timestamp INTEGER NOT NULL,
-  is_deleted INTEGER NOT NULL CHECK (is_deleted IN (0, 1)) DEFAULT 0
+  id VARCHAR(20) NOT NULL PRIMARY KEY,
+  title VARCHAR(200) NOT NULL,
+  wiki_id VARCHAR(50) NOT NULL,
+  category_id VARCHAR(200),
+  creator_username VARCHAR(20),
+  created_timestamp INT UNSIGNED NOT NULL,
+  is_deleted TINYINT(1) NOT NULL CHECK (is_deleted IN (0, 1)) DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS post (
-  id TEXT NOT NULL PRIMARY KEY,
-  thread_id TEXT NOT NULL REFERENCES thread (id),
-  parent_post_id TEXT REFERENCES post (id),
-  posted_timestamp INTEGER NOT NULL,
-  title TEXT NOT NULL,
-  snippet TEXT NOT NULL,
-  user_id TEXT NOT NULL,
-  username TEXT NOT NULL,
-  is_deleted INTEGER NOT NULL CHECK (is_deleted IN (0, 1)) DEFAULT 0
+  id VARCHAR(20) NOT NULL PRIMARY KEY,
+  thread_id VARCHAR(20) NOT NULL REFERENCES thread (id),
+  parent_post_id VARCHAR(20) REFERENCES post (id),
+  posted_timestamp INT UNSIGNED NOT NULL,
+  title VARCHAR(200) NOT NULL,
+  snippet VARCHAR(200) NOT NULL,
+  user_id VARCHAR(20) NOT NULL,
+  username VARCHAR(20) NOT NULL,
+  is_deleted TINYINT(1) NOT NULL CHECK (is_deleted IN (0, 1)) DEFAULT 0
 );
