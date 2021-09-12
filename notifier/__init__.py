62dd500260d4b79df88651e27fdbfb8a929d9f5f
Ross Williams <rossjrw@gmail.com>
2021-09-12 21:58:44 +0100

    Log only to stdout

--- notifier/__init__.py
+++ notifier/__init__.py
@@ -2,14 +2,6 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s",
+    level="DEBUG",
 )
-
-# Log to file at debug level
-file_handler = RotatingFileHandler("notifier.log", backupCount=5)
-
-# TODO Also log at info level to stdout
-# TODO Determine log file location
-# TODO Find a way to rotate the log, if applicable, after the daily
-# channel?
-# TODO Add fuckloads more debug logging
