8a974937e320a09ab0616e5c211c37f8650da8e9
Ross Williams <rossjrw@gmail.com>
2021-09-17 00:07:47 +0100

    Add missing files to ZIP
    My construction method misses a bunch of files that are necessary in the
    project root.

--- zip_lambda.sh
+++ zip_lambda.sh
@@ -1,5 +1,8 @@
 # https://chariotsolutions.com/blog/post/building-lambdas-with-poetry/
+rm lambda.zip -f
 poetry build
 poetry run pip install --upgrade -t build dist/*.whl
-cd build || exit
+cd build || exit 1
 zip -r ../lambda.zip . -x '*.pyc'
+cd .. || exit 1
+zip -u lambda.zip lambda_function.py config.toml auth.lambda.toml
