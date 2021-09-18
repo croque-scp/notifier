3bd16c6b50878c4d80ce075d6cd53ae7a4cf28fa
Ross Williams <rossjrw@gmail.com>
2021-09-18 05:22:51 +0100

    Test timestamp overrides more robustly

--- tests/test_database.py
+++ tests/test_database.py
@@ -189,11 +189,31 @@ def test_deleted_thread(sample_database: BaseDatabaseDriver):
 def test_initial_notified_timestamp(sample_database: MySqlDriver):
     """Test that the initial last notified timestamp for a user is set."""
     with sample_database.transaction() as cursor:
-        cursor.execute(
-            """
-            SELECT notified_timestamp
-            FROM user_last_notified
-            WHERE user_id='1'
-            """
+
+        def check_timestamp(n):
+            cursor.execute(
+                """
+                SELECT notified_timestamp
+                FROM user_last_notified
+                WHERE user_id='1'
+                """
+            )
+            assert (cursor.fetchone() or {})["notified_timestamp"] == n
+
+        check_timestamp(1)
+        sample_database.execute_named(
+            "store_user_last_notified",
+            {
+                "user_id": "1",
+                "notified_timestamp": 2,
+            },
         )
-        assert (cursor.fetchone() or {})["notified_timestamp"] == 1
+        check_timestamp(2)
+        sample_database.execute_named(
+            "store_new_user_last_notified",
+            {
+                "user_id": "1",
+                "notified_timestamp": 3,
+            },
+        )
+        check_timestamp(2)
