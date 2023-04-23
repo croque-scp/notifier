5a5a400ae608434387975d245843efd74d9a75c6
Ross Williams <rossjrw@gmail.com>
2023-04-23 19:03:15 +0100

    Use smallint for log counts for data efficiency

--- notifier/database/migrations/004-store-public-log-dumps.up.sql
+++ notifier/database/migrations/004-store-public-log-dumps.up.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS channel_log_dump (
-  channel                 VARCHAR(10)  NOT NULL, -- like user_config.frequency
-  start_timestamp         INT UNSIGNED NOT NULL UNIQUE,
-  end_timestamp           INT UNSIGNED NOT NULL,
-  user_count              INT          NOT NULL,
-  notified_user_count     INT          NOT NULL,
-  notified_post_count     INT          NOT NULL,
-  notified_thread_count   INT          NOT NULL
+  channel                 VARCHAR(10)       NOT NULL, -- like user_config.frequency
+  start_timestamp         INT UNSIGNED      NOT NULL UNIQUE,
+  end_timestamp           INT UNSIGNED      NOT NULL,
+  user_count              SMALLINT UNSIGNED NOT NULL,
+  notified_user_count     SMALLINT UNSIGNED NOT NULL,
+  notified_post_count     SMALLINT UNSIGNED NOT NULL,
+  notified_thread_count   SMALLINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS activation_log_dump (
-  start_timestamp         INT UNSIGNED NOT NULL UNIQUE,
-  end_timestamp           INT UNSIGNED NOT NULL,
-  sites_count             INT          NOT NULL,
-  user_count              INT          NOT NULL,
-  downloaded_post_count   INT          NOT NULL,
-  downloaded_thread_count INT          NOT NULL
+  start_timestamp         INT UNSIGNED      NOT NULL UNIQUE,
+  end_timestamp           INT UNSIGNED      NOT NULL,
+  sites_count             SMALLINT UNSIGNED NOT NULL,
+  user_count              SMALLINT UNSIGNED NOT NULL,
+  downloaded_post_count   SMALLINT UNSIGNED NOT NULL,
+  downloaded_thread_count SMALLINT UNSIGNED NOT NULL
 );
