18b165fc439b8360167375b4fae60a4dbcb9b713
Ross Williams <rossjrw@gmail.com>
2023-06-05 01:14:05 +0100

    Move Compose tests to MySQL 8
    Also replaces my makeshift DB init script (made for MySQL 5.6, which
    didn't support setup vars) with setup vars.

--- tests/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'root'@'%' IDENTIFIED BY 'root';
-UPDATE mysql.user SET host = '%' WHERE user='root';
-CREATE DATABASE IF NOT EXISTS `wikidot_notifier` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `wikidot_notifier_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-GRANT ALL PRIVILEGES ON `wikidot_notifier_test`.% TO 'root'@'*';
-
-FLUSH PRIVILEGES;
