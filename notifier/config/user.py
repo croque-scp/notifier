e4d96a02d561fdbafab59468b9968078532a65ac
Ross Williams <rossjrw@gmail.com>
2024-05-07 16:50:24 +0100

    Cache user configs fetch
    Speeds up the delete phase. This does mean that user configs will be a
    few mins out of date during the delete phase, but that's probably fine,
    right?

--- notifier/config/user.py
+++ notifier/config/user.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import logging
 from operator import itemgetter
 import re
@@ -77,6 +78,7 @@ def user_config_is_valid(slug: str, config: RawUserConfig) -> bool:
     return ":" in slug and slug.split(":")[1] == config["user_id"]
 
 
+@lru_cache(maxsize=1)
 def fetch_user_configs(
     local_config: LocalConfig, wikidot: Wikidot
 ) -> List[Tuple[str, RawUserConfig]]:
