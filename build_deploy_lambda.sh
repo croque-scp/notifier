a75b74dce65028e83a307d1eecd3e1385108f476
Ross Williams <rossjrw@gmail.com>
2025-01-20 13:01:50 +0000

    Disable provenance attestations
    This results in a total of 3 images being pushed to ECR, only one of
    which is a valid image for Lambda, and it is not the one marked as
    latest.

--- build_deploy_lambda.sh
+++ build_deploy_lambda.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build --target execute_lambda --tag notifier:execute_lambda --tag 288611151503.dkr.ecr.eu-west-2.amazonaws.com/wikidot_notifier:latest .
+docker build --target execute_lambda --tag notifier:execute_lambda --tag 288611151503.dkr.ecr.eu-west-2.amazonaws.com/wikidot_notifier:latest --provenance false .
 aws ecr get-login-password --region eu-west-2 --profile notifier | docker login --username AWS --password-stdin 288611151503.dkr.ecr.eu-west-2.amazonaws.com
 docker push 288611151503.dkr.ecr.eu-west-2.amazonaws.com/wikidot_notifier:latest
 
